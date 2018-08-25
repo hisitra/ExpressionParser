f005e8ec9dbb19a971c00b480e7937334a72ac1f
hisitra <shivanshbox@gmail.com>
2018-08-25 11:01:17 +0530

    Shortened the code, removed some bugs

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import math
 import cmath
 
+class CustomSyntaxError(Exception):
+    pass
+
 class Expression:
 
-    __signs = ["^", "/", "*", "-", "+"]
-    __functions = ["si", "co", "ta", "as", "ac", "at", "lo", "ln"]
-    __numbers = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    __signs = {"^" : lambda a, b : a ** b, "/" : lambda a, b : a / b, "*" : lambda a, b : a * b, "-" : lambda a, b : a - b, "+" : lambda a, b : a + b}
+    __functions = ("si", "co", "ta", "as", "ac", "at", "lo", "ln")
+    __numbers = (".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    __constants = {"e" : math.e, "i" : 1j}
+    __allowedCharacters = {"s", "i", "n", "c", "o", "t", "a", "l", "g", "x", "i", "e", "(", ")"}.union(set(__numbers)).union(set(__signs))
     __functionsDict = {"sin" : cmath.sin, "asin" : cmath.asin, "sinh" : cmath.sinh, "asinh" : cmath.asinh,
                        "cos" : cmath.cos, "acos" : cmath.acos, "cosh" : cmath.cosh, "acosh" : cmath.acosh,
                        "tan" : cmath.tan, "atan" : cmath.atan, "tanh" : cmath.tanh, "atanh" : cmath.atanh,
                        "log" : cmath.log10, "ln" : cmath.log}
 
+
     def __init__(self, exp):
 
-        exp.insert(0, "(")
-        exp.append(")")
-        self.__exp = exp
-        self.__valueAtIndex = [None] * len(self.__exp)
-        self.__endPointOf = [None] * len(self.__exp)
-        self.__assignConstants()
+        try :
 
+            self.__exp = self.__correctSyntax(["("] + list(exp.lower()) + [")"])
+            self.__valueAtIndex = [None] * len(self.__exp)
+            self.__endPointOf = [None] * len(self.__exp)
+            self.__assignConstants()
+
+        except CustomSyntaxError:
+            print("Check syntax")
+            
 
 
     def toString(self):
         return ''.join(self.__exp)
+
+
+    def __correctSyntax(self, exp):
+        for i in range(len(exp)):
+            if exp[i] not in Expression.__allowedCharacters:
+                raise CustomSyntaxError("Check expression syntax")
+            if exp[i] == "(" and exp[i + 1] == "-":
+                exp.insert(i + 1, "0")
+        return exp
 
 
 
@@ -32,31 +50,20 @@ class Expression:
         abb = self.__exp
 
         i = 0
-
         while i < len(abb):
-            
-            if abb[i] == "e":
-
-                self.__valueAtIndex[i] = math.e * (1 + 0j)
+            if abb[i] in Expression.__constants.keys():
+                self.__valueAtIndex[i] = Expression.__constants[abb[i]]
                 self.__endPointOf[i] = i
-
-            if abb[i] == "i":
-
-                self.__valueAtIndex[i] = 0 + 1j
-                self.__endPointOf[i] = i
+                
 
             if abb[i] in Expression.__numbers:
-
                 number = ""
                 startingOfNumber = i
                 while abb[i] in Expression.__numbers:
-
                     number += abb[i]
                     i += 1
-
                 i -= 1
                 endingOfNumber = i
-                
                 self.__valueAtIndex[startingOfNumber] = float(number)
                 self.__valueAtIndex[endingOfNumber] = float(number)
                 self.__endPointOf[startingOfNumber] = endingOfNumber
@@ -71,42 +78,34 @@ class Expression:
         abb = self.__exp
 
         for i in range(len(abb)):
-            
             if abb[i] == "x":
-
                 self.__valueAtIndex[i] = point
                 self.__endPointOf[i] = i
 
         openBracketIndices = []
-
         for i in range(len(abb)):
-
             if abb[i] == "(":
                 openBracketIndices.append(i)
 
             elif abb[i] == ")":
                 self.__valueAtIndex[openBracketIndices[-1]] = self.__expressionParser(openBracketIndices[-1] + 1, i, point)
+                if (self.__valueAtIndex[openBracketIndices[-1]]) == None:
+                    return None
                 self.__valueAtIndex[i] = self.__valueAtIndex[openBracketIndices[-1]]
                 self.__endPointOf[openBracketIndices[-1]] = i
                 self.__endPointOf[i] = openBracketIndices[-1]
                 del openBracketIndices[-1]
 
-##        print()
 ##        print(self.__valueAtIndex)
 ##        print(self.__endPointOf)
-
-
         return self.__valueAtIndex[0]
-            
 
-                    
 
     def __expressionParser(self, start, end, point):
 
         abb = self.__exp
 
         i = start
-
         while i < end:
 
             if abb[i] == "(":
@@ -118,10 +117,15 @@ class Expression:
                 for x in range(i, end):
 
                     if abb[x] == "(":
-                        self.__valueAtIndex[i] = Expression.__functionsDict[''.join(abb)[i : x]](self.__valueAtIndex[x])
+                        try :
+                            self.__valueAtIndex[i] = Expression.__functionsDict[''.join(abb)[i : x]](self.__valueAtIndex[x])
+                        except ValueError:
+                            return None
                         self.__valueAtIndex[self.__endPointOf[x]] = self.__valueAtIndex[i]
                         self.__endPointOf[i] = self.__endPointOf[x]
                         self.__endPointOf[self.__endPointOf[x]] = i
+                        i = x
+                        break
 
             i += 1
 
@@ -135,37 +139,17 @@ class Expression:
 
                 if abb[i] == sign:
 ##                    print(sign, self.__valueAtIndex[i - 1], self.__valueAtIndex[i + 1])
-                    state = True
-                    if sign == "^":
-                        self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__valueAtIndex[i - 1] ** self.__valueAtIndex[i + 1]
-
-                    elif sign == "/":
-                        self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__va
-                        lueAtIndex[i - 1] / self.__valueAtIndex[i + 1]
-
-                    elif sign == "*":
-                        self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__valueAtIndex[i - 1] * self.__valueAtIndex[i + 1]
-
-                    elif sign == "-":
-                        if abb[i - 1] == "(":
-                            self.__valueAtIndex[i] = self.__valueAtIndex[i + 1] * -1
-                            self.__valueAtIndex[self.__endPointOf[i + 1]] = self.__valueAtIndex[i]
-                            self.__endPointOf[i] = self.__endPointOf[i + 1]
-                            self.__endPointOf[self.__endPointOf[i + 1]] = i
-                            state = False
-                        else:
-                            self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__valueAtIndex[i - 1] - self.__valueAtIndex[i + 1]
-
-                    elif sign == "+":
-                        self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__valueAtIndex[i - 1] + self.__valueAtIndex[i + 1]
-
-                    if state:
-                        self.__valueAtIndex[self.__endPointOf[i + 1]] = self.__valueAtIndex[self.__endPointOf[i - 1]]
-                        self.__endPointOf[self.__endPointOf[i - 1]] = self.__endPointOf[i + 1]
-                        if self.__endPointOf[i - 1] == self.__endPointOf[i + 1]:
-                            self.__endPointOf[self.__endPointOf[i + 1]] = i - 1
-                        else:
-                            self.__endPointOf[self.__endPointOf[i + 1]] = self.__endPointOf[i - 1]
+                    try :
+                        self.__valueAtIndex[self.__endPointOf[i - 1]] = self.__signs[abb[i]](self.__valueAtIndex[i - 1], self.__valueAtIndex[i + 1])
+                    except ZeroDivisionError:
+                        return None
+                        
+                    self.__valueAtIndex[self.__endPointOf[i + 1]] = self.__valueAtIndex[self.__endPointOf[i - 1]]
+                    self.__endPointOf[self.__endPointOf[i - 1]] = self.__endPointOf[i + 1]
+                    if self.__endPointOf[i - 1] == self.__endPointOf[i + 1]:
+                        self.__endPointOf[self.__endPointOf[i + 1]] = i - 1
+                    else:
+                        self.__endPointOf[self.__endPointOf[i + 1]] = self.__endPointOf[i - 1]
 
 ##                        print("val[",self.__endPointOf[i + 1],"] = ", self.__valueAtIndex[self.__endPointOf[i + 1]])
 ##                        print("val[",self.__endPointOf[self.__endPointOf[i + 1]],"] = ", self.__valueAtIndex[self.__endPointOf[self.__endPointOf[i + 1]]])
@@ -180,5 +164,5 @@ class Expression:
                 
 
 while True:                
-    expression = Expression(list(input("Expression: ")))
+    expression = Expression(input("Expression: "))
     print(expression.valueAtPoint(1))
